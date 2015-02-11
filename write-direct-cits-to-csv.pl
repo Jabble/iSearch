bc427c3d22c7dd7fa10abb5ddddfcdf1c37e8038
AJ Dabrowska <github@anna.ajdabrowska.net>
2015-02-11 14:09:10 +0100

    Update write-direct-cits-to-csv.pl

--- write-direct-cits-to-csv.pl
+++ write-direct-cits-to-csv.pl
@@ -7,12 +7,18 @@ use warnings;
 # Take directory path and files as commandline input (make sure to include '/' at then end of the path)
 my ($work_space, $direct_cits_txt_file, $direct_cits_csv_file) = @ARGV;
 
+if (not defined $direct_cits_csv_file) {
+  die "Need directory path, direct citation .txt file name, and your .csv filename \n";
+}
+ 
+if (defined $direct_cits_csv_file) {
+  print "Reading '$work_space$direct_cits_txt_file' and writing to '$direct_cits_csv_file'\n";
+}
+
 # Alternatively, define global variables with directory path and files here.
 #my $work_space = "";
 #my $direct_cits_txt_file = "";
 #my $direct_cits_csv_file = "";
-
-
 
 # Open input and output files
 open (INPUT, '<', $work_space.$direct_cits_txt_file) or die "Unable to open $work_space$direct_cits_txt_file\n";
